28670b87a45634733bf595cdad4f8f20009dca0f
Gonzalo Ricco <gonzalo.ricco1@gmail.com>
2021-08-15 17:43:44 -0600

    Add movile-nav to packages and customers page

--- shared.js
+++ shared.js
@@ -1,1 +1,15 @@
 // alert('hola')
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+var backdrop = document.querySelector('.backdrop')
+
+toggleButton.addEventListener('click',function() {
+  console.log('sdfdf')
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+})
+
+backdrop.addEventListener('click', function(){
+  mobileNav.classList.remove('open')
+  backdrop.classList.remove('open')
+})
